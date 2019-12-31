d075cb2ec12cb39d359a9fcb1a1063943e8f5600
congdoan6997 <congdoan6997@gmail.com>
2019-12-31 17:42:51 +0300

    delete complete and delete all

--- todo/urls.py
+++ todo/urls.py
@@ -4,5 +4,7 @@ from django.urls import path,include
 urlpatterns = [
     path('', views.index, name='index'),
     path('add',views.addTodo,name='add'),
-    path('complete/<todo_id>',views.completeTodo,name='complete')
+    path('complete/<todo_id>',views.completeTodo,name='complete'),
+    path('deleteComplete', views.deleteComplete,name='deleteComplete'),
+    path('deleteAll', views.deleteAll,name='deleteAll')
 ]
